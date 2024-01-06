163b88323254bd77f01060f8b5abef2915079305
chytika <chaudharytikaram03@gmail.com>
2024-01-06 16:25:55 +0545

    flash message

--- website/userauths/views.py
+++ website/userauths/views.py
@@ -16,7 +16,7 @@ def Register_view(request):
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"hey {{username}}, your account is created successfully")
+            messages.success(request, f"hey {username}, your account is created successfully")
             new_user = authenticate(username=form.cleaned_data["email"], password=form.cleaned_data["password1"])
             login(request, new_user)
             return  redirect("core:index")
