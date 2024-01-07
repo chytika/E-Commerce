9196711d7cd9dfbdfb14862d2ddaaa345b3528dd
chytika <chaudharytikaram03@gmail.com>
2024-01-07 16:24:21 +0545

    list view page

--- website/core/views.py
+++ website/core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from core.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Address, Wishlist
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    products = Product.objects.all()
+
+    context = {
+        "products":products
+    }
+    return render(request, 'core/index.html', context)
